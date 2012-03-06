0581656c4cd806230d470d5ddb4c90deefec4eee
Stephan Wallentin <stephan.wallentin@ef.com>
2012-03-06 11:45:12 +0800

    update some scripts and added bash stuff

--- chuckbook.local.sh
+++ chuckbook.local.sh
@@ -1,5 +1,3 @@
-unset $CHECKMAIL
-
 # local environment exports
 
 export webroot="/Library/WebServer/Documents"
@@ -16,4 +14,16 @@ alias development="cd ~/development"
 alias dropbox="cd ~/Dropbox"
 alias scripts="cd ~/scripts"
 
+alias gb="git branch"
+alias gba="git branch -a"
+alias gc="git commit -v"
+alias gd="git diff | mate"
+alias gl="git pull"
+alias gp="git push"
+alias gst="git status"
+
+PS1='\[\033[01;31m\][ \h ] \[\033[01;32m\][ \u ]\033[01;36m\] [ \w ]\[\033[01;37m\] $ \[\033[00m\]'
+
 git config --global core.editor "mate -w"
+
+unset $CHECKMAIL
